cbe2b5b7dfa2120b6abd2b92de13c5219651ce29
giosue2812 <giosue_liuzzo@hotmail.be>
2021-03-25 16:14:48 +0100

    Mise a jour de la dal Interfaces en cours

---
+++ DAL_Movie/Repositories/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_Movie.Repositories.Interfaces
+{
+    public interface IRepositoryBase<TEntity>
+    {
+        IEnumerable<TEntity> GetAll();
+        TEntity Get(int id);
+        TResult Create<TResult, TBody>(TBody entity);
+        TResult Update<TResult, TBody>(TBody entity);
+        TResult Delete<TResult>(int id);
+    }
+}
